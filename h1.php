ce87e0939ca3ff081778e193716892cc952d565e
hombig56 <25994@srithatpit.ac.th>
2025-01-17 11:29:09 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+    <p>-----การแสดงข้อความแบบหัวข้อ-----</p>
+    <div>
+        <h1>HTML เบื้องต้น(h1)</h1>
+    <div>
+        <h2>HTML เบื้องต้น(h1)</h2>
+        <h3>HTML เบื้องต้น(h1)</h3>
+        <h4>HTML เบื้องต้น(h1)</h4>
+        <h5>HTML เบื้องต้น(h1)</h5>
+        <h6>HTML เบื้องต้น(h1)</h6>
+    <div>
+        <p>----การแสดงตัวหนา ตัวเอียง การเขียดเส้นใต้ข้อความ----</p>
+    <div>
+     <b>ธนาวัฒน์ คำแก้ว</b><br>
+     <i>ธนาวัฒน์ คำแก้ว</i><br>
+     <u>ธนาวัฒน์ คำแก้ว</u><br>
+     <div>
+         <p><b>-----การ Link-----</b></p>
+    <div>
+        <!--Linkหน้าเดียว-->
+        <a href = "https://www.google.co.th">Google</a><br>
+    <div>
+
+</body>
+</html>
